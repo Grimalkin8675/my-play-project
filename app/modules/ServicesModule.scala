135730f0c4fff8a6d0698e170cc0ba9c802a5aa6
Axel Nussbaumer <axel.nussbaumer@eisti.fr>
2019-01-31 13:04:08 +0100

    remove DummyInventoryService

--- app/modules/ServicesModule.scala
+++ app/modules/ServicesModule.scala
@@ -5,7 +5,6 @@ import play.api.inject.Module
 
 import controllers.{WriteService, QueryHandler}
 import services.{
-  DummyInventoryService,
   EventsService,
   ProductsProjection,
   EventBus,
@@ -15,8 +14,6 @@ import services.{
 
 class ServicesModule extends Module {
   def bindings(env: Environment, conf: Configuration) = Seq(
-    // bind[WriteService].to[DummyInventoryService],
-    // bind[QueryHandler].to[DummyInventoryService],
     bind[WriteService].to[EventsService],
     bind[QueryHandler].to[ProductsProjection],
     bind[Publishable].to[EventBus],
